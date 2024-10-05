9093b25d633f7a79fe215744bb0500e43bdbd2d9
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-10-05 19:50:48 -0400

    update

--- script.js
+++ script.js
@@ -116,5 +116,61 @@ const handleSearchByCategory = () => {
 
 }
 
+const handleLatestPosts = async() => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const data = await response.json();
+    displayLatestPosts(data);
+    // displayAllPosts(data.posts);
+}
+
+handleLatestPosts();
+
+const displayLatestPosts = (latestPosts) =>{
+    console.log(latestPosts);
+    const latestPostContainer = document.getElementById('latest-post-container');
+    latestPosts.forEach(latestPost => {
+        const div = document.createElement('div');
+        div.innerHTML = 
+        `
+        <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+          <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+            <img src=${latestPost.cover_image} alt="Shoes" class="rounded-xl" />
+          </figure>
+          <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+            <p class="opacity-50 text-start">
+              <i class="fa-solid fa-calendar-days me-2"></i>${latestPost.author.posted_date?latestPost.author.posted_date:"No Publish Date"}
+            </p>
+            <h2 class="card-title text-start">${latestPost.title}</h2>
+            <p class="text-start">
+              ${latestPost.description}
+            </p>
+            <div class="card-actions flex gap-5 items-center">
+              <div class="avatar">
+                <div class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src=${latestPost.profile_image} />
+                </div>
+              </div>
+              <div>
+                <h3 class="text-start font-extrabold">${latestPost.author.name}</h3>
+                <p class="text-start opacity-60">${latestPost.author.designation?latestPost.author.designation:'Unknown'}</p>
+              </div>
+            </div>
+
+
+            <span id="latestPostLoader" class="loading loading-infinity loading-lg lg:mt-24 text-primary hidden">
+
+
+
+
+
+            </span>
+            <!-- dynamic content -->
+          </div>
+        </div>
+        `
+        latestPostContainer.appendChild(div);
+
+    })
+}
 
 loadAllPosts();
